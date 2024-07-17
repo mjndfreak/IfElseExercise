d0c62e4d9c40fd3d09957734ebde8469aa8ed562
Cagdas <cagda@CAGDAS>
2024-07-17 12:40:55 +0300

    Add project files.

---
+++ IfElseExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+// I take a number imput from the user.
+// Convert it to int and save it to number.
+Console.Write("Enter a number:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+//Checks the number and outputs the number's status.
+//First checks if the number is negative.
+if (number < 0)
+{
+    Console.WriteLine("Invalid number!");
+}
+
+else  if (number < 10)
+{
+    Console.WriteLine("Number is smaller than 10.");
+}
+
+else if (number > 10)
+{
+    Console.WriteLine("Number is greater than 10.");
+}
+
+else
+{
+    Console.WriteLine("Number equals to 10.");
+}
